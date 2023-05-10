4f6fff4917707ff022eb1fde288c6bc8be840367
Leon <anthonygarayleon@gmail.com>
2023-05-10 19:45:45 -0300

    Spring git commit -m Spring

---
+++ src/test/java/Inmobilaria/GyL/GyLApplicationTests.java
@@ -0,0 +1,13 @@
+package Inmobilaria.GyL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
