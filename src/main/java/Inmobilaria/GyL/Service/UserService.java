486cdc29f8c9cee46b899228d31d60f6c2399038
Daniela <daniela.morales.it@gmail.com>
2023-05-11 20:52:44 -0300

    cambios en el userService

--- src/main/java/Inmobilaria/GyL/Service/UserService.java
+++ src/main/java/Inmobilaria/GyL/Service/UserService.java
@@ -2,7 +2,12 @@ package Inmobilaria.GyL.Service;
 
 import Inmobilaria.GyL.Entities.ImageUser;
 import Inmobilaria.GyL.Entities.User;
+import Inmobilaria.GyL.Enums.Role;
 import Inmobilaria.GyL.Repositories.UserRepository;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +18,34 @@ public class UserService  {
     @Autowired
     private UserRepository userRepository;
     
+    
+     @Transactional
+    public void createUser(String email, String password, String name) {
+
+        User user = new User();
+
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setName(name);
+        user.setRole(Role.CLIENT);
+        user.setCreateDate(Date.from(Instant.MIN));
+
+        userRepository.save(user);
+    }
+
+    public User getOne(Long id) {
+        return userRepository.getOne(id);
+    }
+
+    public List<User> listUsers() {
+        List<User> users = new ArrayList();
+        users = userRepository.findAll();
+
+        return users;
+    }
+
     @Transactional
-    public void ModifyUser(Long id,String name,String email,String password,ImageUser icon){
+    public void modifyUser(Long id,String name,String email,String password,ImageUser icon){
     
         Optional<User> response= userRepository.findById(id);
         if(response.isPresent()){ 
