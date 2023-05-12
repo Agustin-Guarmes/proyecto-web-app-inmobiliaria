fc13e57630fa1ce2369010e2e22fb425c93dec13
Daniela <daniela.morales.it@gmail.com>
2023-05-11 22:48:34 -0300

    eliminar

--- src/main/java/Inmobilaria/GyL/service/impl/PropertyService.java
+++
@@ -1,8 +0,0 @@
-package Inmobilaria.GyL.service.impl;
-
-import Inmobilaria.GyL.service.IPropertyService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class PropertyService implements IPropertyService {
-}
