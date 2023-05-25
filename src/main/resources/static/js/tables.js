08c143af8d175c8483588a72c97d2799ff0f9520
Agustin-Guarmes <agustinguarmes@gmail.com>
2023-05-25 12:59:16 -0300

    tema claro/oscuro

---
+++ src/main/resources/static/js/tables.js
@@ -0,0 +1,16 @@
+$('#example').DataTable({
+    responsive: true,
+    autoWidth: false,
+    "language": {
+        "lengthMenu": "Mostrar _MENU_ registros por página",
+        "zeroRecords": "Nada encontrado - disculpa",
+        "info": "Página _PAGE_ de _PAGES_",
+        "infoEmpty": "No records available",
+        "infoFiltered": "(filtrado de _MAX_ registros totales)",
+        "search": "Buscar:",
+        "paginate" : {
+            "previous": "Anterior",
+            "next": "Siguiente",
+        }
+    }
+});
---
+++ src/main/resources/static/js/theme.js
@@ -0,0 +1,6 @@
+const clearTheme = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "light");
+}
+const darkTheme = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "dark");
+}
