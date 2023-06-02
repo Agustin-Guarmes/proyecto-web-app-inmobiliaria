a094913686e895614587ab213ee50c49fa7a7972
Agustin-Guarmes <agustinguarmes@gmail.com>
2023-06-02 20:27:36 -0300

    error 400

--- src/main/resources/static/js/formValidation.js
+++ src/main/resources/static/js/formValidation.js
@@ -38,3 +38,25 @@ confirmPasswordInput.addEventListener('input', () => {
     error2Feedback.classList.add('d-none');
   }
 });
+
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+
+const regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+const regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
+
+emailInput.addEventListener('input', () => {
+  if (!regExEmail.test(emailInput.value)) {
+    emailInput.setCustomValidity('Correo electrónico inválido');
+  } else {
+    emailInput.setCustomValidity('');
+  }
+});
+
+passwordInput.addEventListener('input', () => {
+  if (!regExPass.test(passwordInput.value)) {
+    passwordInput.setCustomValidity('La contraseña debe tener entre 8 y 20 caracteres, al menos una letra mayúscula, una letra minúscula y un dígito.');
+  } else {
+    passwordInput.setCustomValidity('');
+  }
+});
